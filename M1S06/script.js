fd5de0dde65b641d59f9ac3610e87fb368757c17
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-02-25 23:04:52 -0300

    feat: ex9 e 10 - adiciona logo, funcao de validacao no nome

--- M1S06/script.js
+++ M1S06/script.js
@@ -3,15 +3,18 @@ let nomeUsuario;
 function dispararBoasVindas() {
     nomeUsuario = document.getElementById("nome");
     alert('Seja bem vindo(a) ' + nomeUsuario.innerText + "!");
-
 }
 
 function alterarNome() {
     nomeUsuario = document.getElementById("nome");
-
-    const novoNome = prompt('Digite um Nome:')
-
-    nomeUsuario.innerText = novoNome;
+    const novoNome = prompt('Digite um Nome:');
+    const nomeValidacao = parseInt(novoNome);
+    
+    if (!isNaN(nomeValidacao)){
+        alterarNome();
+    }else {
+        nomeUsuario.innerText = novoNome;
+    }
 }
 
 function alterarDescricao() {
