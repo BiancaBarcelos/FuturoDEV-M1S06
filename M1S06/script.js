291aa34d982ae9ad095a9d7a087723c06f1092f6
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-02-25 22:28:35 -0300

    feat: funcao de alteracao nome e descricao

--- M1S06/script.js
+++ M1S06/script.js
@@ -1,4 +1,22 @@
-function bemVindo() {
-    alert('Seja bem vindo(a)!');
+let nomeUsuario;
 
+function dispararBoasVindas() {
+    nomeUsuario = document.getElementById("nome");
+    alert('Seja bem vindo(a) ' + nomeUsuario.innerText + "!");
+
+}
+
+function alterarNome() {
+    nomeUsuario = document.getElementById("nome");
+
+    const novoNome = prompt('Digite um Nome:')
+
+    nomeUsuario.innerText = novoNome;
+}
+
+function alterarDescricao() {
+    const desc = document.getElementById('descricao');
+    const novaDesc = prompt('Digite uma Descrição:')
+
+    desc.innerText = novaDesc;
 }
